6f36a38ac7baa7133873509f3447c08ae04c3808
elisavetterz <eterzani.atm@gmail.com>
2024-03-31 16:44:48 +0200

    One way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  // name: string = 'Elisavet';
-  name = 'Elisavet';
+  name = 'Christodoulos';
 
   person = {
-    givenName: 'Elis',
+    givenName: 'Elisavet',
     surName: 'Terz',
     age: 0x37,
     email: 'savet@aueb.gr',
   };
-
 }

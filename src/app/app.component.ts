2028fe7afbe2ecf42dc8aa73e8af7709573dade7
elisavetterz <eterzani.atm@gmail.com>
2024-03-31 10:44:49 +0200

    Component Person Creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { Person } from './shared/interfaces/person';
+import { PersonTableComponent } from "./components/person-table.component";
+// import { PersonTableComponent } from './components/person-table/person-table.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [PersonTableComponent]
 })
 export class AppComponent {
   name: string = 'Elisavet';
+
+  person0: Person = {
+    givenName: 'Christodoulos',
+    surName: 'Fragkoudakis',
+    age: 0x37,
+    email: 'chfrag@aueb.gr',
+    address: 'Athens, Greece',
+  };
+
+  person1: Person = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 0x25,
+    email: 'john@example.com',
+    address: 'New York, USA',
+  };
 }

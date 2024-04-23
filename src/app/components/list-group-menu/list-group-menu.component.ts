0521e729c02ba24db516291b288bb3a9017f29be
elisavetTerz <eterzani.atm@gmail.com>
2024-04-23 19:31:22 +0200

    crud-example add to list

--- src/app/components/list-group-menu/list-group-menu.component.ts
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -46,6 +46,7 @@ export class ListGroupMenuComponent {
     {
       text: 'Fun For Nerds Game',
       routerLink: 'fun-for-nerds'
-    }
+    },
+    { text: 'CRUD Example', routerLink: 'crud-example' },
   ];
 }

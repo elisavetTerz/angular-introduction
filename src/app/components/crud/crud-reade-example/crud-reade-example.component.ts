2a16c29f93be98cf446e008a4cbf8dcd67013a81
elisavetTerz <eterzani.atm@gmail.com>
2024-04-23 19:27:31 +0200

    crud-example

---
+++ src/app/components/crud/crud-reade-example/crud-reade-example.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CrudNavbarComponent } from '../crud-navbar/crud-navbar.component';
+
+@Component({
+  selector: 'app-crud-reade-example',
+  standalone: true,
+  imports: [CrudNavbarComponent],
+  templateUrl: './crud-reade-example.component.html',
+  styleUrl: './crud-reade-example.component.css'
+})
+export class CrudReadeExampleComponent {
+
+}
